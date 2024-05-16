f5c8d4aff7326f542f1317c096ef1c7140760b32
Ali Yorulmaz <aliyorulmazdev@gmail.com>
2024-05-16 14:57:18 +0300

    - Update image rendering and add filigran overlay.

--- app/_components/ZoomImage.js
+++ app/_components/ZoomImage.js
@@ -14,19 +14,30 @@ const ZoomImage = ({ src, alt, className, width, height }) => {
 
   return (
     <div className="relative overflow-hidden shadow-lg stroke-transparent">
-      <Image
-        alt={alt}
-        className={`w-full h-full ${className}`}
-        src={src}
-        width={width}
-        height={height}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        style={{
-          transform: isHovered ? "scale(1.1)" : "scale(1)",
-          transition: "transform 0.5s ease-in-out",
-        }}
-      />
+      <div className="relative">
+        <Image
+          alt={alt}
+          className={`w-full h-full ${className}`}
+          src={src}
+          width={width}
+          height={height}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={{
+            transform: isHovered ? "scale(1.1)" : "scale(1)",
+            transition: "transform 0.5s ease-in-out",
+          }}
+        />
+          <div className="absolute bottom-0 right-0">
+            <Image
+              alt="Filigran"
+              src="/images/izmir.svg" // Filigran görüntüsünün yolu
+              width={width / 3} // Filigran boyutu ayarlayabilirsiniz
+              height={height / 3}
+              className="opacity-25" // İsteğe bağlı: filigranı daha az belirgin yapmak için opaklık ayarı
+            />
+          </div>
+      </div>
     </div>
   );
 };
