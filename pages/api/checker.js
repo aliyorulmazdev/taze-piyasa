3ba7db96c6d34448795c6ac801ef32f055c68f42
aliyorulmazdev <aliyorulmazdev@gmail.com>
2024-05-16 21:22:21 +0300

    Added delay to mails.

--- pages/api/checker.js
+++ pages/api/checker.js
@@ -1,25 +1,20 @@
 import axios from 'axios';
 import { Resend } from 'resend';
-import prisma from '../../prisma/client/db.js'
+import prisma from '../../prisma/client/db.js';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
-    // GET isteği olup olmadığını kontrol etme
     if (req.method === 'GET') {
         try {
-            // Tarih bilgisini al
             const formattedDate = new Date().toISOString().split('T')[0];
-            // API'den veriyi al
             const response = await axios.get(`https://openapi.izmir.bel.tr/api/ibb/halfiyatlari/sebzemeyve/${formattedDate}`);
             const data = response.data;
 
             const reminders = await prisma.PriceReminder.findMany();
 
             for (const row of reminders) {
-                // console.log(row)
                 for (const item of data.HalFiyatListesi) {
-                    // console.log(item.MalAdi)
                     if (item.MalAdi === row.urunAdi) {
                         let price;
                         if (row.fiyatTipi === 'Average') {
@@ -33,26 +28,23 @@ export default async function handler(req, res) {
                         if (price <= row.istenenFiyat) {
                             // E-posta gönderimi
                             await sendEmail(row.mailAdresi);
-                            // console.log(`${row.adSoyad} için eposta gönderildi.`);
                             // İlgili veritabanı satırını silme işlemi
                             await prisma.PriceReminder.delete({ where: { id: row.id } });
+                            // 2 saniye bekleme
+                            await delay(2000);
                         }
                     }
                 }
             }
 
-            // Prisma bağlantısını kapat
             await prisma.$disconnect();
 
-            // Başarılı yanıt gönderme
             res.status(200).json({ message: 'İşlem tamamlandı.' });
         } catch (error) {
             console.error('İstek hatası:', error);
-            // Hata durumunda yanıt gönderme
             res.status(500).json({ error: 'Internal Server Error' });
         }
     } else {
-        // Desteklenmeyen HTTP metodu durumunda yanıt gönderme
         res.status(405).json({ error: 'Method Not Allowed' });
     }
 }
@@ -70,4 +62,8 @@ async function sendEmail(mail) {
     }
 
     console.log('E-posta gönderildi:', data);
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
